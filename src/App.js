2997c55f8a33c34711d953d0554eaf93f82ed57b
SiyabongaNzulwana <nzulwanas@yahoo.com>
2018-06-02 14:14:14 +0200

    work so far

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
 import React, { Component } from 'react';
+import request from 'request';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       response: ''
     }
+    this.callApi = this.callApi.bind(this);
+    this.changeState = this.changeState.bind(this);
   }
   componentDidMount() {
-    this.callApi()
-      .then(res => this.setState({ response: res.express }))
-      .catch(err => console.log(err));
+    const urls = ['https://prima.run/health','https://cognition.dev.stackworx.cloud/api/status','https://api.durf.dev.stackworx.io/health','https://stackworx.io/'];
+    setInterval(() => {
+      return this.callApi(urls)
+        .then(res => {
+          console.log("Stats code: ", res)
+          this.setState({ response: res }, () => console.log("Respoinse: ", this.state.response))
+        })
+        .catch(err => console.log(err));
+    }, 3000)
   }
 
-  callApi = async () => {
-    const response = await fetch('/api/hello');
-    const body = await response.json();
+  changeState(response) {
+    if (response.statusCode === 200) {
+      this.setState({ response }, () => localStorage.setItem('checkResult', JSON.stringify(response.statusCode)  ))
+    }else{
 
-    if (response.status !== 200) throw Error(body.message);
+    };
+  }
 
-    return body;
-  };
+  callApi = async urls =>
+  urls.map(url => request(url, (error, response, body) => {
+      console.log('error:', error);
+      console.log('statusCode:', response && response.statusCode);
+      console.log('body:', body);
+      return {
+        response
+      };
+    }).on('response', response => {
+      console.log("This: ", this)
+      this.changeState(response);
+    })
+  );
   render() {
     return (
       <div className="App">
@@ -31,7 +53,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-        {this.state.response}
+          {this.state.response.hostname}
         </p>
       </div>
     );
